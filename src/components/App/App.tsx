3b951f2b604d16fdfe24e995455efc3947417f23
Joao Victor Fanchini de Azevedo <joaofanchini@gmail.com>
2021-04-16 16:28:10 -0300

    Add . project after reset

---
+++ src/components/App/App.tsx
@@ -0,0 +1,63 @@
+import React, { useState } from 'react';
+import './App.css';
+import Header from '../Header';
+import Container from '../../shared/Container';
+import Table, { TableHeader } from '../../shared/Table';
+import Products, { Product } from '../../shared/Table/Table.mockdata';
+import ProductForm, { ProductCreator } from '../Products/ProductForm';
+
+const headers: TableHeader[] = [
+  { key: 'id', value: '#' },
+  { key: 'name', value: 'Product' },
+  { key: 'price', value: 'Price', right: true },
+  { key: 'stock', value: 'Available Stock', right: true }
+]
+
+function App() {
+  const [products, setProducts] = useState(Products)
+  const [updatingProduct, setUpdatingProduct] = useState<Product | undefined>(products[0])
+  
+  const handleProductSubmit = (product: ProductCreator) => {
+    setProducts([
+      ...products,
+      {
+        id: products.length + 1,
+        ...product
+      }
+    ])
+  }
+
+  const handleProductUpdate = (newProduct: Product) => {
+    setProducts(products.map(product =>
+      product.id === newProduct.id
+        ? newProduct
+        : product
+    ))
+
+    setUpdatingProduct(undefined)
+  }
+
+  return (
+    <div className="App">
+      <Header title="AlgaStock" />
+      <Container>
+        <Table
+          headers={headers}
+          data={products}
+          enableActions
+          onDelete={console.log}
+          onDetail={console.log}
+          onEdit={console.log}
+        />
+
+        <ProductForm
+          form={updatingProduct}
+          onSubmit={handleProductSubmit}
+          onUpdate={handleProductUpdate}
+        />
+      </Container>
+    </div>
+  );
+}
+
+export default App;
